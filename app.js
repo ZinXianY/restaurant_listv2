1b3e43ad653d2cf610457b56db566a7b47de2248
Zin <tyulkeds159357@gmail.com>
2021-05-01 22:42:40 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 //載入工具
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
+//設定連線到mongodb
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+//取得資料庫連線狀態
+const db = mongoose.connection
+
+//連線異常
+db.on('err', () => {
+  console.log('mongodb error!')
+})
+
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 //設定首頁路由
 app.get('/', (req, res) => {
